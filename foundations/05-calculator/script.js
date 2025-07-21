6561fe730715098f69b8410ec09b3b56cfc1af7a
fazetdev <fazetdev@google.com>
2025-07-21 22:29:37 +0100

    add html, css, and js files for the calculator project

---
+++ foundations/05-calculator/script.js
@@ -0,0 +1,25 @@
+function add(a, b) {
+   let number = a+b
+   console.log(number)
+}
+
+add(123, 123)
+
+function subtract(a, b){
+   let number = a-b
+   console.log(number)
+}
+subtract(123, 123)
+
+function multiply(a, b) {
+   let number = a*b
+   console.log(number)
+}
+multiply(123, 123)
+
+function divide(a, b) {
+   let number = a/b
+   console.log(number)
+}
+divide(123, 123)
+
